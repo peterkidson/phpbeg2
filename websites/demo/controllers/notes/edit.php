40be980c1b2d79e375ffbfd38080b2470d745dd2
Peter Kidson <peterdkidson@googlemail.com>
2023-08-08 06:02:07 +0100

    36 3:00

---
+++ websites/demo/controllers/notes/edit.php
@@ -0,0 +1,9 @@
+<?php
+
+
+view('notes/edit.view.php', [  // <form method="POST" action="/notes">
+	'heading' 	=> 'Edit Note',
+	'errors'		=> []
+]);
+
+
--- websites/demo/views/notes/show.view.php
+++ websites/demo/views/notes/show.view.php
@@ -12,25 +12,29 @@
 			<?= $note['id'] ?>  <?= htmlspecialchars($note['body']) ?>
 		</p>
 
-		<form class="mt-3" method="post">
-			<div class="shadow sm:overflow-hidden sm:rounded-md">
-				<input type="hidden" name="_pseudoMethod" value="DELETE">
-				<input type="hidden" name="idFromTheDeleteForm" value="<?= $note['id']; ?>">
-<!--				<button class="text-sm text-red-500">Delete from form</button>-->
+		<footer class="mt-3">
+			<a href="/note/edit">Edit</a>
+		</footer>
 
-				<button type="submit"
-						  class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white
-										  	shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-					Delete from form
-				</button>
-
-			</div>
-		</form>
-
+<!--		<form class="mt-3" method="post">-->
+<!--			<div class="shadow sm:overflow-hidden sm:rounded-md">-->
+<!--				<input type="hidden" name="_pseudoMethod" value="DELETE">-->
+<!--				<input type="hidden" name="idFromTheDeleteForm" value="--><?php //= $note['id']; ?><!--">-->
+<!--				<button type="submit"-->
+<!--						  class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white-->
+<!--										shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">-->
+<!--					Delete from form-->
+<!--				</button>-->
+<!--			</div>-->
+<!--		</form>-->
+<!---->
+<!--
 		<p class="mt-6 text-sm text-red-500">
-			<?php $ref="/zap?id=".$note['id']; ?>
+			<here ?php $ref="/zap?id=".$note['id']; ?>
 			<a href=<?= $ref ?>>Delete from link</a>
 		</p>
+-->
+
 
 	</div>
 </main>
