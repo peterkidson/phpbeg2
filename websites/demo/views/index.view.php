dfecd525b3d53f0feae950ad6db45d6140a147a6
Peter Kidson <peterdkidson@googlemail.com>
2023-09-09 11:30:15 +0100

    41 Logging out

--- websites/demo/views/index.view.php
+++ websites/demo/views/index.view.php
@@ -2,7 +2,6 @@
 <?php require "partials/nav.php"; ?>
 <?php require "partials/banner.php"; ?>
 
-<?php echo "hello"; ?>
 <main>
 	<div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
 		Hello <i><?= $_SESSION['user']['email'] ?? 'Guest' ?></i>
--- websites/demo/views/partials/nav.php
+++ websites/demo/views/partials/nav.php
@@ -56,7 +56,7 @@
 
 						<div class="relative ml-3">
 							<form method="POST" action="/session">
-								<input type="hidden" name="_pseudomethod" value="DELETE"/>
+								<input type="hidden" name="_pseudoMethod" value="DELETE"/>
 
 								<button class="text-white">Log Out</button>
 							</form>
@@ -73,27 +73,6 @@
 
 					<?php endif; ?>
 
-						<!--
-						  Dropdown menu, show/hide based on menu state.
-
-						  Entering: "transition ease-out duration-100"
-							 From: "transform opacity-0 scale-95"
-							 To: "transform opacity-100 scale-100"
-						  Leaving: "transition ease-in duration-75"
-							 From: "transform opacity-100 scale-100"
-							 To: "transform opacity-0 scale-95"
-						-->
-
-<!--						<div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5-->
-<!--							focus:outline-none"  role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">-->
-							<!-- Active: "bg-gray-100", Not Active: "" -->
-<!--							<a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"-->
-<!--								id="user-menu-item-0">Your Profile</a>-->
-<!--							<a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"-->
-<!--								id="user-menu-item-1">Settings</a>-->
-<!--							<a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"-->
-<!--								id="user-menu-item-2">Sign out</a>-->
-<!--						</div>-->
 					</div>
 				</div>
 			</div>
@@ -118,7 +97,6 @@
 				</button>
 			</div>
 		</div>
-	</div>
 
 	<!-- Mobile menu, show/hide based on menu state. -->
 	<div class="md:hidden" id="mobile-menu">
@@ -166,6 +144,9 @@
 					class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign
 					out</a>
 			</div>
+
 		</div>
+
 	</div>
+
 </nav>
