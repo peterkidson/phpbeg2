7c9a46e92f21635f458c9131aad14ff8654d319f
Peter Kidson <peterdkidson@googlemail.com>
2023-09-09 09:03:55 +0100

    41 ~16:00  fix nav

--- websites/demo/views/partials/nav.php
+++ websites/demo/views/partials/nav.php
@@ -39,37 +39,39 @@
 					</button>
 
 					<!-- Profile dropdown -->
-					<div class="relative ml-3">
-						<div>
+					<?php if ($_SESSION['user'] ?? false) : ?>
+
+						<div class="relative ml-3">
 							<button type="button"
 									  class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 									  id="user-menu-button" aria-expanded="false" aria-haspopup="true">
 
 								<span class="sr-only">Open user menu</span>
 
-								<?php if ($_SESSION['user'] ?? false) : ?>
 									<img class="h-8 w-8 rounded-full"
 										  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
 										  alt="">
-								<?php else : ?>
-									<a href="/login"
-										class="<?= uriIs('/login') ? 'bg-gray-900 text-white' : 'text-gray-300' ?>
-													hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log in</a>
-									<a href="/register"
-										class="<?= uriIs('/register') ? 'bg-gray-900 text-white' : 'text-gray-300' ?>
-													hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Register</a>
-								<?php endif; ?>
 							</button>
 						</div>
 
-						<?php if($_SESSION['user'] ?? false) : ?>
-							<div class="ml-3">
-								<form method="POST" action="/session">
-									<input type="hidden" name="_method" value="DELETE" />
-									<button class="text-white">Log out</button>
-								</form>
-							</div>
-						<?php endif; ?>
+						<div class="relative ml-3">
+							<form method="POST" action="/sessions">
+								<input type="hidden" name="_method" value="DELETE"/>
+
+								<button class="text-white">Log Out</button>
+							</form>
+						</div>
+
+					<?php else : ?>
+
+						<a href="/register"
+							class="<?= uriIs('/register') ? 'bg-gray-900 text-white' : 'text-gray-300' ?>
+										hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Register</a>
+						<a href="/login"
+							class="<?= uriIs('/login') ? 'bg-gray-900 text-white' : 'text-gray-300' ?>
+										hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log in</a>
+
+					<?php endif; ?>
 
 						<!--
 						  Dropdown menu, show/hide based on menu state.
