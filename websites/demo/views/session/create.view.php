ae98d861f1f177a93d550275798d5ee337f5608c
Peter Kidson <peterdkidson@googlemail.com>
2023-10-11 06:20:49 +0100

    45 Done

--- websites/demo/views/session/create.view.php
+++ websites/demo/views/session/create.view.php
@@ -11,13 +11,14 @@
 				<h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Log In</h2>
 			</div>
 
+
 			<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 				<form class="space-y-6" action="/session" method="POST">
 					<div>
 						<label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
 						<div class="mt-2">
 							<input id="email" name="email" type="email" autocomplete="email" required
-									 value="<?= Core\Session::get('old')['email'] ?? ''?>"
+									 value="<?= old('email') ?>"
 									 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
 									 			placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
 						</div>
